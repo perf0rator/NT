e6294264f8b18fc4143b409722c7693dcd38011e
Andrey <a-nm.ya@ya.ru>
2017-11-06 03:01:57 +0300

    added nearest neighbors search

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import tornado.ioloop
 import tornado.web
 from datetime import datetime
-from urllib.parse import urlparse
 from bson.json_util import dumps
 from math import sqrt
 import json
@@ -11,99 +10,96 @@ import pymongo
 MONGODB_HOST = "127.0.0.1"
 MONGODB_PORT = 27017
 
+
 class Home(tornado.web.RequestHandler):
 
     def initialize(self):
-        '''self.conn = pymongo.MongoClient(MONGODB_HOST, MONGODB_PORT)
-        self.db = self.conn['blogger']
-        self.blogs = self.db['blogs']'''
-
         self.conn = pymongo.MongoClient(MONGODB_HOST, MONGODB_PORT)
-        self.db = self.conn['users']
+        self.db = self.conn['points']
         self.coords = self.db['coords']
 
 
-class User(Home):
+class Point(Home):
 
-    def get(self, usid):
+    def get(self, pid):
         try:
-            user = self.db.users.find_one({"_id": int(usid)})
+            point = self.db.points.find_one({"_id": int(pid)})
             self.set_header('Content-Type', 'application/json')
-            self.write(dumps(user))
+            self.write(dumps(point))
         except TypeError:
             self.write('788778')
 
     def post(self):
-        _id = self.db.users.count() + 1
+        _id = self.db.points.count() + 1
         timestamp = datetime.now()
 
         self.set_header("Content-Type", "application/json")
         #data = json.loads(self.get_body_arguments('x'))
 
-        user = {
+        point = {
                 "_id": _id,
                 "x": self.get_query_argument('x'),
                 "y": self.get_query_argument('y'),
                 "timestamp": timestamp
         }
-        self.db.users.insert(user)
-        location = "/user/" + str(_id)
+        self.db.points.insert(point)
+        location = "/point/" + str(_id)
         self.set_header('Content-Type', 'application/json')
         self.set_header('Location', location)
         self.set_status(201)
-        self.write(dumps(user))
+        self.write(dumps(point))
 
-    def put(self, usid):
-        ## Convert unicode to int
-        _id = int(usid)
+    def put(self, pid):
+        _id = int(pid)
         timestamp = datetime.now()
-        user = {
+        point = {
                 "x": self.get_query_argument('x'),
                 "y": self.get_query_argument('y'),
                 "timestamp": timestamp
         }
-        self.db.users.update({"_id": _id}, {"$set": user})
+        self.db.points.update({"_id": _id}, {"$set": point})
         self.set_header('Content-Type', 'application/json')
-        self.write(dumps(user))
+        self.write(dumps(point))
 
-    def delete(self, usid):
-        ## Convert unicode to int
-        _id = int(usid)
-        user = {
+    def delete(self, pid):
+        _id = int(pid)
+        point = {
                 "_id": None,
                 "x": None,
                 "y": None,
                 "timestamp": None,
         }
-        self.db.users.update({"_id": _id}, {"$set": user})
+        self.db.points.update({"_id": _id}, {"$set": point})
         self.set_header('Content-Type', 'application/json')
-        self.write(dumps(user))
+        self.write(dumps(point))
 
-class Users(Home):
+
+class Points(Home):
 
     def get(self):
-        users = str(list(self.db.users.find()))
+        points = str(list(self.db.points.find()))
         self.set_header('Content-Type', 'application/json')
-        self.write(dumps(users))
+        self.write(dumps(points))
 
     def delete(self):
-        users = str(list(self.db.users.find()))
+        points = str(list(self.db.points.find()))
         self.set_header('Content-Type', 'application/json')
-        self.db.users.drop()
-        self.write(dumps(users))
+        self.db.points.drop()
+        self.write(dumps(points))
+
 
 class Distance(Home):
 
     def get(self):
         location = "/dist/"
-        user1_id = self.get_query_argument('user1')
-        user2_id = self.get_query_argument('user2')
-        user1 = self.db.users.find_one({"_id": int(user1_id)})
-        user2 = self.db.users.find_one({"_id": int(user2_id)})
-        x1 = int(user1['x'])
-        y1 = int(user1['y'])
-        x2 = int(user2['x'])
-        y2 = int(user2['y'])
+        point1_id = self.get_query_argument('point1')
+        point2_id = self.get_query_argument('point2')
+        point1 = self.db.points.find_one({"_id": int(point1_id)})
+        point2 = self.db.points.find_one({"_id": int(point2_id)})
+        x1 = int(point1['x'])
+        y1 = int(point1['y'])
+        x2 = int(point2['x'])
+        y2 = int(point2['y'])
         dist = sqrt((x2 - x1)**2 + (y2-y1)**2)
         print(x1, y1, x2, y2)
         print(dist)
@@ -111,12 +107,11 @@ class Distance(Home):
         self.write(dumps(dist))
 
 
-
 application = tornado.web.Application([
     (r"/", Home),
-    (r"/user/([0-9]+)", User),
-    (r"/user/", User),
-    (r"/users/", Users),
+    (r"/point/([0-9]+)", Point),
+    (r"/point/", Point),
+    (r"/points/", Points),
     (r"/dist/", Distance)
     ], debug=True)
 
