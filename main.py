2426bebef68f068b5b60be80a88abf59f238da58
Andrey <a-nm.ya@ya.ru>
2017-11-27 02:57:38 +0300

    added create_point func, another changes

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ from tornado import gen
 from datetime import datetime
 from bson.json_util import dumps
 from bson.son import SON
-
 import random
 
 MONGODB_HOST = "127.0.0.1"
@@ -18,6 +17,15 @@ db = motor.MotorClient(MONGODB_HOST, MONGODB_PORT).points
 class Home(tornado.web.RequestHandler):
     def init(self):
         self.render("samples.html")
+
+    def create_point(self):  # not a final version of the func
+        x = float(self.get_query_argument('x'))
+        y = float(self.get_query_argument('y'))
+        if x and y:
+            if -90 < x < 90 and -180 < y < 180:
+                return [x, y]
+            else:
+                return None
 
 
 class Point(Home):
@@ -34,22 +42,21 @@ class Point(Home):
 
     @gen.coroutine
     def put(self, pid):
+        self.set_header('Content-Type', 'application/json')
+        xy = self.create_point()
         timestamp = str(datetime.now())
-        x = int(self.get_query_argument('x'))
-        y = int(self.get_query_argument('y'))
-        if x and y:
-            if -90 < x < 90 and -180 < y < 180:
+        try:
+            if xy is not None:
                 point = {
-                    "point": [x, y],
+                    "point": xy,
                     "timestamp": timestamp
                 }
                 yield db.points.update({"_id": int(pid)}, {"$set": point})
-                self.set_header('Content-Type', 'application/json')
                 self.write(dumps(point))
             else:
                 self.set_status(400)
                 self.write(dumps("coordinates must be in range x = [-90, 90], y = [-180, 180]"))
-        else:
+        except:
             self.set_status(400)
             self.write(dumps('Missing argument x or y'))
 
@@ -81,19 +88,19 @@ class Points(Home):
 
     @gen.coroutine
     def post(self):
+        self.set_header('Content-Type', 'application/json')
         timestamp = datetime.now()
         time = int(datetime.timestamp(timestamp))
         rand = random.randint(1, 1000)
         _id = time * 10 ** 4 + rand   # smekalochka
-        x = int(self.get_query_argument('x'))
-        y = int(self.get_query_argument('y'))
-
-        if x and y:
-            if -90 < x < 90 and -180 < y < 180:
+        xy = self.create_point()
+        timestamp = str(datetime.now())
+        try:
+            if xy is not None:
                 point = {
                     "_id": _id,
-                    "point": [x, y],
-                    "timestamp": str(timestamp)
+                    "point": xy,
+                    "timestamp": timestamp
                 }
                 yield db.points.insert(point)
                 self.set_status(201)
@@ -101,7 +108,7 @@ class Points(Home):
             else:
                 self.set_status(400)
                 self.write(dumps("coordinates must be in range x = [-90, 90], y = [-180, 180]"))
-        else:
+        except:
             self.set_status(400)
             self.write(dumps('Missing argument x or y'))
 
@@ -116,7 +123,8 @@ class Points(Home):
 class FindKnn(Home):
     @gen.coroutine
     def get(self, pid, n):
-        r = int(self.get_query_argument('r'))
+        self.set_header('Content-Type', 'application/json')
+        r = float(self.get_query_argument('r'))
         n = int(self.get_query_argument('n'))
         point = yield db.points.find_one({"_id": int(pid)})
         xy = point["point"]
@@ -126,6 +134,8 @@ class FindKnn(Home):
             document = points.next_object()
             self.write(dumps(document))
             self.set_status(200)
+
+
 
 
 application = tornado.web.Application([
